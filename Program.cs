0ae9ee84452b4637453b7c6f85829b816716c876
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-18 13:59:39 +0530

    []Rupali]ADDED: Welcome to Logical programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
